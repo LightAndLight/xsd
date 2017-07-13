c2b8b668d6ffbe83428210c0ada90fb72e7f58fc
Isaac Elliott <isaace71295@gmail.com>
2017-07-13 15:44:39 +1000

    Converted Boolean to use `parsers` and `attoparsec`

--- src/Text/XML/Decimal.hs
+++ src/Text/XML/Decimal.hs
@@ -6,6 +6,7 @@ module Text.XML.Decimal
   ( Decimal
   , isDecimal
   , mkDecimal
+  , parseDecimal
   , dc
   , _Decimal
   )
@@ -13,13 +14,17 @@ module Text.XML.Decimal
 
 import Prelude
 
+import Control.Applicative ((<|>))
 import Control.Lens (Prism', prism')
-import Data.Char (isDigit)
+import Data.Attoparsec.Text (parseOnly)
+import Data.Functor (($>))
 import Data.Maybe (isJust)
 import Data.Monoid
 import Data.Text (Text)
 import Language.Haskell.TH.Quote
 import Language.Haskell.TH.Syntax
+import Text.Parser.Char
+import Text.Parser.Combinators
 
 import qualified Data.Text as T
 
@@ -45,18 +50,30 @@ instance Lift Decimal where
       r' = T.unpack r
     in [| Decimal s (T.pack l') (T.pack r') |]
 
-isDecimal :: String -> Bool
+isDecimal :: Text -> Bool
 isDecimal = isJust . mkDecimal
 
-mkDecimal :: String -> Maybe Decimal
-mkDecimal "" = Nothing
-mkDecimal (x:xs) = _
+mkDecimal :: Text -> Maybe Decimal
+mkDecimal a =
+  case parseOnly parseDecimal a of
+    Right d -> Just d
+    _ -> Nothing
+
+parseDecimal :: CharParsing m => m Decimal
+parseDecimal =
+  let
+    sign = try (char '+' $> Pos) <|> try (char '-' $> Neg) <|> pure Pos
+  in Decimal <$>
+     sign <*>
+     fmap T.pack (some digit) <*>
+     (try (char '.' *> fmap T.pack (many digit)) <|> pure "0") <*
+     eof
 
 dc :: QuasiQuoter
 dc =
   QuasiQuoter
   { quoteExp = \str ->
-      case mkDecimal str of
+      case mkDecimal (T.pack str) of
         Just d -> [| d |]
         Nothing -> fail $ str <> " is not a valid Decimal"
   , quotePat = error "`dc` cannot be used as a pattern"
@@ -68,4 +85,4 @@ _Decimal :: Prism' Text Decimal
 _Decimal =
   prism'
   (\(Decimal s l r) -> showSign s <> l <> "." <> r)
-  (mkDecimal . T.unpack)
+  mkDecimal

1c31fa2ae1f7d7885b0e5d1be9127887b33eabec
Isaac Elliott <isaace71295@gmail.com>
2017-07-13 12:46:35 +1000

    Fixed type of id'

--- src/Text/XML/ID.hs
+++ src/Text/XML/ID.hs
@@ -1,8 +1,10 @@
 module Text.XML.ID where
 
+import Prelude (Maybe)
+
 import Control.Lens (Lens')
 
 import Text.XML.NCName
 
 class HasID s where
-  id' :: Lens' s NCName
+  id' :: Lens' s (Maybe NCName)

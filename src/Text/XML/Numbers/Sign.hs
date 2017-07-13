14bbee94f2e74bd78fcbccb84acd6b7d3b21e0fc
Isaac Elliott <isaace71295@gmail.com>
2017-07-13 15:51:33 +1000

    Added Numbers.Sign

---
+++ src/Text/XML/Numbers/Sign.hs
@@ -0,0 +1,22 @@
+{-# language DeriveLift #-}
+{-# language OverloadedStrings #-}
+
+module Text.XML.Numbers.Sign where
+
+import Control.Applicative ((<|>), pure)
+import Data.Functor (($>))
+import Data.Text (Text)
+import Language.Haskell.TH.Syntax
+import Text.Parser.Char
+import Text.Parser.Combinators
+
+data Sign = Pos | Neg deriving Lift
+
+showSign :: Sign -> Text
+showSign a =
+  case a of
+    Pos -> "+"
+    Neg -> "-"
+
+sign :: CharParsing m => m Sign
+sign = try (char '+' $> Pos) <|> try (char '-' $> Neg) <|> pure Pos

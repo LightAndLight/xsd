23a8fad1b57af94b6c0d808b08f8e7461986243f
Isaac Elliott <isaace71295@gmail.com>
2017-07-14 15:00:35 +1000

    Converted NonNegative to use `parsers` and `attoparsec`

--- src/Text/XML/NonNegative.hs
+++ src/Text/XML/NonNegative.hs
@@ -12,29 +12,36 @@ module Text.XML.NonNegative
 import Prelude
 
 import Control.Lens (Prism', prism')
-import Data.Char (isDigit)
+import Data.Attoparsec.Text (parseOnly)
+import Data.Maybe (isJust)
 import Data.Monoid
 import Data.Text (Text)
 import Language.Haskell.TH.Quote
 import Numeric.Natural
+import Text.Parser.Char
+import Text.Parser.Combinators
 
 import qualified Data.Text as T
 
 type NonNegative = Natural
 
-isNonNegative :: String -> Bool
-isNonNegative = all isDigit
+isNonNegative :: Text -> Bool
+isNonNegative = isJust . mkNonNegative
 
-mkNonNegative :: String -> Maybe NonNegative
-mkNonNegative a
-  | isNonNegative a = Just $ read a
-  | otherwise = Nothing
+mkNonNegative :: Text -> Maybe NonNegative
+mkNonNegative a =
+  case parseOnly parseNonNegative a of
+    Right n -> Just n
+    _ -> Nothing
+
+parseNonNegative :: CharParsing m => m NonNegative
+parseNonNegative = read <$> some digit
 
 nn :: QuasiQuoter
 nn =
   QuasiQuoter
   { quoteExp = \str ->
-      case mkNonNegative str of
+      case mkNonNegative (T.pack str) of
         Just n -> [| n |]
         Nothing -> fail $ str <> " is not a valid NonNegative"
   , quotePat = error "`nn` cannot be used as a pattern"
@@ -43,4 +50,4 @@ nn =
   }
 
 _NonNegative :: Prism' Text NonNegative
-_NonNegative = prism' (T.pack . show) (mkNonNegative . T.unpack)
+_NonNegative = prism' (T.pack . show) mkNonNegative

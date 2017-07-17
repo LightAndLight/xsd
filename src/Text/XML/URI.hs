7b7a35feea997c3be956270da5b8b6f89027aa28
Isaac Elliott <isaace71295@gmail.com>
2017-07-17 13:49:55 +1000

    Fixed types in URI module

--- src/Text/XML/URI.hs
+++ src/Text/XML/URI.hs
@@ -1,7 +1,7 @@
 {-# language TemplateHaskell #-}
 
 module Text.XML.URI
-  ( URI
+  ( URI.URI
   , isURI
   , mkURI
   , uri
@@ -15,21 +15,24 @@ import Control.Lens (Prism', prism')
 import Data.Monoid
 import Data.Text (Text)
 import Language.Haskell.TH.Quote
-import Network.URI
 
 import qualified Data.Text as T
+import qualified Network.URI as URI
 
-mkURI :: String -> Maybe URI
-mkURI = parseURI
+isURI :: Text -> Bool
+isURI = URI.isURI . T.unpack
+
+mkURI :: Text -> Maybe URI.URI
+mkURI = URI.parseURI . T.unpack
 
 uri :: QuasiQuoter
 uri =
   QuasiQuoter
   { quoteExp = \str ->
-      case mkURI str of
-        Just (URI s Nothing p q f) -> [| URI s Nothing p q f |]
-        Just (URI s (Just (URIAuth u r po)) p q f)
-          -> [| URI s (Just (URIAuth u r po)) p q f |]
+      case mkURI (T.pack str) of
+        Just (URI.URI s Nothing p q f) -> [| URI.URI s Nothing p q f |]
+        Just (URI.URI s (Just (URI.URIAuth u r po)) p q f)
+          -> [| URI.URI s (Just (URI.URIAuth u r po)) p q f |]
         Nothing
           -> fail $ str <> " is not a valid URI"
   , quotePat = error "`uri` cannot be used as a pattern"
@@ -37,5 +40,5 @@ uri =
   , quoteDec = error "`uri` cannot be used as a declaration"
   }
 
-_URI :: Prism' Text URI
-_URI = prism' (T.pack . ($ "") . uriToString id) (mkURI . T.unpack)
+_URI :: Prism' Text URI.URI
+_URI = prism' (T.pack . ($ "") . URI.uriToString id) mkURI
